029bc5e85d8fb7b5f8691a78d505e840778ae575
limpa95 <patrickhlim@gmail.com>
2025-04-03 22:13:14 -0500

    Changed scrapper address in DeckPage to custom domain

--- frontend/src/pages/DeckPage.js
+++ frontend/src/pages/DeckPage.js
@@ -40,7 +40,7 @@ function DeckPage({ setCard }) {
     // Make POST request to partner's microservice using card name and receive document id containing img url
     let id = '';
     const cardImg = async () => {
-        const response = await fetch('http://164.152.30.119:4000/findCard', {
+        const response = await fetch('http://patrickhlim.com:4000/findCard', {
             
             method: 'POST',
             body: JSON.stringify({'name': name}),
