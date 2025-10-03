9461d19a9ad03d4cef980bcd4d70fae9bed317dd
limpa95 <patrickhlim@gmail.com>
2025-10-03 16:06:17 -0500

    Changed scrapper website

--- yugioh-web-scrapper-edited/utils/scrapper.js
+++ yugioh-web-scrapper-edited/utils/scrapper.js
@@ -7,49 +7,27 @@ module.exports = async function findCard(searchString) {
     var newSearchString = "";
     for(var i=0; i<searchString.length; i++) {
         if(searchString[i] == ' ') {
-            newSearchString += '%20';
+            newSearchString += '-';
         } else {
             newSearchString += searchString[i];
         }
     }
-    url = 'https://ygoprodeck.com/card-database/?&name=' + newSearchString + '&num=100&offset=0';
+    url = 'https://formatlibrary.com/cards/' + newSearchString;
     await page.goto(url);
 
     await new Promise(r => setTimeout(r, 500));
 
-    await page.evaluate(() => new Promise((resolve) => {
-        var scrollTop = -1;
-        const interval = setInterval(() => {
-          window.scrollBy(0, 100);
-          if(document.documentElement.scrollTop !== scrollTop) {
-            scrollTop = document.documentElement.scrollTop;
-            return;
-          }
-          clearInterval(interval);
-          resolve();
-        }, 10);
-      }));
-
-    await new Promise(r => setTimeout(r, 1500));
-
-    const cards = await page.evaluate(() => 
-        Array.from(document.querySelectorAll('#api-area-results .item-area'), (e) => ({
-            name: e.querySelector('.item-name h1').innerText,
-            description: e.querySelector('.item-ability p').innerText,
-            imagePath: e.querySelector('.item-img img').getAttribute('src')
-        }))
-    )
-    var res = null;
-    for (let i = 0; i < cards.length; i++) {
-        if(cards[i].name == searchString) {
-            res = cards[i];
-        }
-    }
     
-    if(res==null) {
-        res = cards[0];
-    }
+    let name = await page.$eval('.single-card-title.pwk-border-bottom', (e) => e.innerText)
+    let description = await page.$eval('.single-card-description-box', (e) => e.innerText)
+    let crdPic = await page.$eval('.vertical-centered-flexbox > img', (e) => e.getAttribute('src'))
+
+    const cards = {
+        name: name,
+        description: description,
+        imagePath: crdPic
+    };
 
     await browser.close();
-    return res;
+    return cards;
 }
